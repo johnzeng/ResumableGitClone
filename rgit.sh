bebcfebac0d77f1ac95e99747324de20f6d5f361
zengda <zengda@yunba.io>
2016-12-28 14:21:55 +0800

    bug fix

--- rgit.sh
+++ rgit.sh
@@ -4,14 +4,21 @@ clone_str=`git clone --depth 1 $1 2>&1|grep Cloning`
 echo $clone_str
 [[ "$clone_str" =~ \'(.*)\' ]] && work_dir=${BASH_REMATCH[1]}
 
+if [[ "$work_dir" == "" ]]
+then
+    echo "fail to clone"
+    exit 1
+fi
+
 cd $work_dir
 pwd
 step=1
-while 1
+
+while true
 do
     echo "depth is $step"
     fetch_str=`git fetch --depth $step`
-    if [ "$fetch_str" -eq "remote: Total 0 (delta 0), reused 0 (delta 0), pack-reused 0"]
+    if [[ "$fetch_str" -eq "remote: Total 0 (delta 0), reused 0 (delta 0), pack-reused 0" ]]
     then
         break
     fi
